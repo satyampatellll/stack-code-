9446f3464aba1f320b05897aee16a3169abb839c
satyampatellll <satyampatel251317@gmail.com>
2026-06-14 21:53:02 +0530

    TransferStack

---
+++ TransferStack.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+
+public class TransferStack {
+    public static void main(String[] args) {
+        Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+
+        s1.push(10);
+        s1.push(20);
+        s1.push(30);
+
+        while (!s1.isEmpty()) {
+            s2.push(s1.pop());
+        }
+
+        System.out.println("Stack 2: " + s2);
+    }
+}
